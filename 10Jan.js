4cea3e167e3df9a9475600202d58a648a95e67b5
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-10 08:58:30 -0700

    first binary challenge complete

---
+++ 10Jan.js
@@ -0,0 +1,18 @@
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary
+//  representation of that number. You can guarantee that input is non-negative.
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+const translateBinary = (integer) => {
+  if (integer === 0) return 0;
+  let binaryResult = [];
+  while(integer >= 1) {
+    if (integer % 2 === 0) {
+      binaryResult.unshift(0);
+      integer = integer/2;
+    } else {
+      binaryResult.unshift(1);
+      integer = Math.floor(integer/2);
+    }
+  }
+  return parseInt(binaryResult.join(''));
+}
